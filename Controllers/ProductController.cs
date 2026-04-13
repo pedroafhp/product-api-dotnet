b500412aaa8f185e781d632dcb32ef2650e8c3fa
Pedro Henrique Felix de Almeida <pedro.almeida@civiloc.com.br>
2026-04-13 14:31:51 -0300

    Adiciona gitignore

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,32 @@
+using Microsoft.AspNetCore.Mvc;
+using ProductApi.Data;
+using ProductApi.Models;
+
+namespace ProductApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public ProductController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok(_context.Products.ToList());
+        }
+
+        [HttpPost]
+        public IActionResult Create(Product product)
+        {
+            _context.Products.Add(product);
+            _context.SaveChanges();
+            return Ok(product);
+        }
+    }
+}
